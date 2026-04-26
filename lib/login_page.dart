2f6d8d1d58b5d2e52a03d1a8f1f88dcd7211e919
ahnafmohsin114 <ahnafmohsin428@email.com>
2026-04-26 21:08:59 +0600

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: (){
+                print("Email: ${emailController.text}");
+                print("Password: ${passwordController.text}");
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Login Successful")),
+                );
+                
+              },
+              child: Text("LOGIN"),
+            ),
+          ],
+        ),
+      ),
+    );
+
+  }
+  
+}
